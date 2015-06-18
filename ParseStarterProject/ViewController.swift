ecda173f11d2b447c00c8af312b1d16213e81b73
roodoodey <mathieugrettir@hotmail.com>
2015-06-18 19:49:48 +0000

    Initial Commit

---
+++ ParseStarterProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseStarterProject
+//
+//  Created by Mathieu Skulason on 18/06/15.
+//  Copyright (c) 2015 Mathieu Skulason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
